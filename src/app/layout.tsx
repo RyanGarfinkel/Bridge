fb0ecb1542b2b313e90af2fbeed3ec0e033bd306
RyanGarfinkel <113050972+RyanGarfinkel@users.noreply.github.com>
2025-04-28 21:39:34 -0400

    change icon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,9 +29,7 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <title>Bridgly</title>
-        <link rel="icon" href="/favicon.ico" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="icon" type="image/png" href="/icon.png" />
       </head>
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         <DataProvider>
